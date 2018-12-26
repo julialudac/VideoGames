6656ce74a34a82b6fea08732a1795b8e38f9668b
DESKTOP-CIJ17O9\julia <julialudac@yahoo.fr>
2018-12-25 18:36:57 -0800

    Class ExtractTrainValidateTest

--- Train_n_test.py
+++ Train_n_test.py
@@ -55,3 +55,46 @@ class TrainValidateTest:
         output_df.to_csv("test_labels.CSV", index=False)
 
 
+class ExtractTrainValidateTest:
+    """
+    1/ Extract and process DataFrames
+    2/ Train and validate => it will yield an accuracy
+    This class is not really to output estimations about players' ids but more used to find the best
+    hyperparameters: t, n_estimators, max_depth, df_training, df_validation to get the best accuracy.
+    Then, when you are happy with the parameters, you can use them into notebook
+    "extracting_training_validating_testing" to output results for a test dataset in a csv file.
+    """
+
+    def __init__(self):
+        self.tnv = None
+
+    def extract_and_process_df(self, t):
+        """
+        :param t: The time limit where to stop extracting the data. When not a multiple of 5, it is rounded to
+        upper 5.
+        """
+        # Prepare train and test original datasets (+ labels)
+        self.df_training = de.get_dataframe("../input/minitrain.CSV", training=True, limit_seconds=t)  # OK
+        self.df_validation = de.get_dataframe("../input/minitest.CSV", training=True, limit_seconds=t)  # OK
+
+        # Encoding
+        # -- Learning dataset encoding
+        self.encoder = ThreeFeaturesEncoder(self.df_training)
+        # -- Encoding dataset : training and validation
+        self.encoded_df_training = self.encoder.encode_df(self.df_training)
+        self.encoded_df_validation = self.encoder.encode_df(self.df_validation)
+
+        # Getting the datasets we're gonna work on: Convert dataset into another one
+        self.df_training_numerical = de.transform_sample(self.encoded_df_training, True)
+        self.df_validation_numerical = de.transform_sample(self.encoded_df_validation, True)
+
+        # Conform validation DataFrame columns to training DataFrame columns
+        self.df_validation_numerical = de.conform_test_to_training(self.df_training_numerical,
+                                                                   self.df_validation_numerical)
+        self.df_validation_numerical = self.df_validation_numerical.fillna(0)
+
+    def train_and_validate(self, n_estimators, max_depth):
+        self.tnv = TrainValidateTest(n_estimators, max_depth)
+        self.tnv.train(self.df_training_numerical)
+        self.tnv.validate(self.df_validation_numerical)
+
